29b4617c14d6d58b266fa8982087c6eecb9ff3a2
khs0806 <boydsa12@naver.com>
2020-01-28 15:28:43 +0000

    myvenv 삭제

--- teststst/settings.py
+++ teststst/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0opg&^5zc48)m6w0ew0dti_x%2d)rlq$&kh@ehj^7=21rgn1&l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
 
 
 # Application definition
